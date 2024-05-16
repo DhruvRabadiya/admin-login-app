39b304c5959a39b973786e35ac0fb73e64d879b8
Dhruv Rabadiya <dhrvrabadiya@gmail.com>
2024-05-16 10:50:39 +0530

    Model created and seeding is done

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,19 +3,12 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
-
     /**
      * Define the model's default state.
      *
@@ -24,21 +17,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'full_name' => fake()->name, // Generating full name using the fake() function
+            'user_name' => fake()->userName, // Generating user name using the fake() function
+            'email' => fake()->unique()->safeEmail, // Generating email using the fake() function
+            'password' => bcrypt('password'), // You may generate a hashed password or use a plain text one for testing purposes
+            'mobile_number' => fake()->phoneNumber, // Generating phone number using the fake() function
+            'date_of_birth' => fake()->date($format = 'Y-m-d', $max = '2000-01-01'),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
