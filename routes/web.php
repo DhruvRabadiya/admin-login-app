62c1e2f8dd563064b4d67a3982c6dba66c585c56
Dhruv Rabadiya <dhrvrabadiya@gmail.com>
2024-05-16 09:13:52 +0530

    route and controller created

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
-
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login' , [LoginController::class ,'loginPage']);
