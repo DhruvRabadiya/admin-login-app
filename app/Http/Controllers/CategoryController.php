401aafd9440004e4af1cbb91657f694b15016dd4
Dhruv Rabadiya <dhrvrabadiya@gmail.com>
2024-05-30 11:39:24 +0530

    category and parent category is shown in datatable

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,13 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Yajra\DataTables\DataTables;
+
 
 class CategoryController extends Controller
 {
-    public function category(){
+    public function category(Request $request)
+    {
         $user = Auth::user();
-        return view('category' , compact('user'));
+
+        if ($user) {
+            if ($request->ajax()) {
+                $data = Category::with('parentcategory')->select(['id', 'category_name', 'parent_id', 'url', 'description']);
+                return Datatables::of($data)
+                    ->addIndexColumn()
+                    ->addColumn('parent_category', function ($row) {
+                        return $row->parentcategory ? $row->parentcategory->category_name : 'N/A';
+                    })
+                    ->addColumn('action', function ($data) {
+                        $editButton = '<button type="button" class="editBtn btn btn-primary btn-sm ml-2" data-id="' . $data->id . '">Edit</button>';
+                        $deleteButton = '<button type="button" class="deleteBtn btn btn-danger btn-sm ml-2" data-id="' . $data->id . '">Delete</button>';
+                        return $editButton . $deleteButton;
+                    })
+                    ->rawColumns(['action'])
+                    ->make(true);
+            }
+            $categories = Category::with('parentcategory')->get()->toArray();
+            return view('category', compact('user', 'categories'));
+        }
+        return redirect()->route('profile');
     }
 }
--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -32,12 +32,29 @@
             <section class="content">
                 <div class="container-fluid">
                     <div class="row">
-                        <div class="col-md-3">
+                        <div class="col-md-12">
                             <div class="card card-primary card-outline">
                                 <div class="card-body box-profile">
 
 
-
+                                    <div class="text-center">
+                                        <section class="content">
+                                            <table class="table table-bordered" id="tables_data">
+                                                <thead>
+                                                    <tr>
+                                                        <th>No</th>
+                                                        <th>Name</th>
+                                                        <th>Parent Category</th>
+                                                        <th>URL</th>
+                                                        <th>Description</th>
+                                                        <th>Action</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                </tbody>
+                                            </table>
+                                        </section>
+                                    </div>
 
                                 </div>
                                 <!-- /.card -->
@@ -56,4 +73,72 @@
 
     </div>
     <!-- ./wrapper -->
+@endsection
+
+
+@section('scripts')
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <!-- DataTables -->
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <!-- AdminLTE App -->
+    {{-- <script src="{{ asset('js/adminlte.min.js') }}"></script> --}}
+    <!-- SweetAlert -->
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+
+    <script>
+        $(document).ready(function() {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+
+            var table = $('#tables_data').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: "{{ route('category') }}",
+                columns: [{
+                        data: 'DT_RowIndex',
+                        name: 'DT_RowIndex'
+                    },
+                    {
+                        data: 'category_name',
+                        name: 'category_name'
+                    },
+                    {
+                        data: 'parent_category',
+                        name: 'parentcategory.category_name'
+                    },
+                    {
+                        data: 'url',
+                        name: 'url'
+                    },
+                    {
+                        data: 'description',
+                        name: 'description'
+                    },
+
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: false,
+                        searchable: false
+                    }
+                ],
+                columnDefs: [{
+                    targets: 0,
+                    searchable: false,
+                    orderable: false,
+                    render: function(data, type, full, meta) {
+                        return meta.row + 1;
+                    }
+                }]
+            });
+        });
+    </script>
 @endsection
